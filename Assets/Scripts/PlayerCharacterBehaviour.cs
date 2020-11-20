99dffffa9e7c75f5d5b1f0e08fa8948dbf87a577
David <daveski084@users.noreply.github.com>
2020-11-20 15:19:54 -0500

    added main menu

--- Assets/Scripts/PlayerCharacterBehaviour.cs
+++ Assets/Scripts/PlayerCharacterBehaviour.cs
@@ -1,7 +1,28 @@
-﻿using System.Collections;
+﻿/***********************************************************************;
+* Project            : Tsumuji's Winter Adventure
+*
+* Author             : David Gasinec
+* 
+* Student Number     : 101187910
+*
+* Date created       : 20/11/20
+*
+* Description        : Enum for animation states.
+*
+* Last modified      : 20/11/20
+*
+* Revision History   :
+*
+* Date        Author Ref    Revision (Date in YYYYMMDD format) 
+* 201120    David Gasinec        Created script. 
+*
+*
+|**********************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/** Player Character Movement. */
 public class PlayerCharacterBehaviour : MonoBehaviour
 {
     public Joystick joystick;
