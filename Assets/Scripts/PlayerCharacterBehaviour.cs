863809a68399103c3f0fef8b47af26eeecbf715f
David <daveski084@users.noreply.github.com>
2020-11-19 19:24:16 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerCharacterBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterBehaviour : MonoBehaviour
+{
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+
+    private Rigidbody2D m_rigidBody2D;
+    private SpriteRenderer m_spriteRenderer;
+    private Animator m_animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (joystick.Horizontal > joystickHorizontalSensitivity)
+        {
+            // Move right.
+            m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+            m_animator.SetInteger("Animation State", (int)PlayerAnimationState.RUN);
+            m_spriteRenderer.flipX = false;
+        }
+        else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+        {
+            // Move left.
+            m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+            m_animator.SetInteger("Animation State", (int)PlayerAnimationState.RUN);
+            m_spriteRenderer.flipX = true;
+        }
+        else
+        {
+            m_animator.SetInteger("Animation State", (int)PlayerAnimationState.IDLE);
+        }
+    }
+}
