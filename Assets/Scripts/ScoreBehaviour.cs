558c829aac42dd84b3f539b742d7e2e0ab694c02
David <daveski084@users.noreply.github.com>
2020-12-11 19:31:31 -0500

    Added Timer

---
+++ Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,44 @@
+﻿/***********************************************************************;
+* Project            : Tsumuji's Winter Adventure
+*
+* Author             : David Gasinec
+* 
+* Student Number     : 101187910
+*
+* Date created       : 20/12/10
+*
+* Description        : Controls score logic.
+*
+* Last modified      : 20/12/11
+*
+* Revision History   :
+*
+*Date        Author Ref    Revision (Date in YYYYMMDD format) 
+*201024    David Gasinec        Created script. 
+*
+|**********************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+/** Handles score logic. */
+public class ScoreBehaviour : MonoBehaviour
+{
+
+    public static int scoreNumber = 0;
+    Text score;
+
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    // Updates the score.
+    void Update()
+    {
+        score.text = "Score: " + scoreNumber;
+    }
+}
---
+++ Assets/Scripts/TimerBehaviour.cs
@@ -0,0 +1,56 @@
+﻿/***********************************************************************;
+* Project            : Tsumuji's Winter Adventure
+*
+* Author             : David Gasinec
+* 
+* Student Number     : 101187910
+*
+* Date created       : 20/12/10
+*
+* Description        : Controls timer logic.
+*
+* Last modified      : 20/12/11
+*
+* Revision History   :
+*
+*Date        Author Ref    Revision (Date in YYYYMMDD format) 
+*201024    David Gasinec        Created script. 
+*
+|**********************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/** Handles timer logic. */
+public class TimerBehaviour : MonoBehaviour
+{
+    public float time = 10;
+    public Text timerText;
+
+    void Update()
+    {
+        
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            DisplayTime(time);
+        }
+        else
+        {
+            Debug.Log("Time has run out!");
+            time = 0;
+        }
+        
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
